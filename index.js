a466c3dade2d45ab6c2e0cfd6545b1eb3d37151f
AryanK-IT <aryankumar.it04@gmail.com>
2025-07-09 21:13:09 +0530

    2nd commit

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!'); // This is a simple GET endpoint that responds with "Hello World!"
+  res.send('Hello World!'); 
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`); // This logs the server URL to the console
+  console.log(`Server is running at http://localhost:${port}`); 
 });
-
-// This code sets up a basic Express server that listens on port 3000 and responds to GET requests at the root URL with "Hello World!". 
